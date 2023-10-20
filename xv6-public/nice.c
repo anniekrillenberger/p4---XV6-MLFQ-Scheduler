1eb6a2bf2258220cac9aa7478c496eff18da8d26
anniekrillenberger <krillenberge@wisc.edu>
2023-10-20 16:25:51 -0500

    syscall setup & required Makefile changes

---
+++ xv6-public/nice.c
@@ -0,0 +1,12 @@
+
+/**
+ * nice() should set the current process's nice value to
+ * n and return the previous nice value, or -1 on error. 
+*/
+int main(int argc, char *argv[]) 
+{
+    int niceVal;
+    int prevNiceVal;
+    niceVal = nice(prevNiceVal);
+
+} 
